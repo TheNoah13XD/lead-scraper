4fe6eb025e14d08b309cb4222063198c670e8a5c
TheNoah13XD <broken.personal.1211@gmail.com>
2024-11-18 23:08:39 +0530

    feat: get profile and social links

--- src/routes.ts
+++ src/routes.ts
@@ -6,18 +6,51 @@ router.addDefaultHandler(async ({ request, page, log }) => {
     const pageTitle = await page.title();
     log.info(`URL: ${request.url}, TITLE: ${pageTitle}`);
 
-    const pageContent = await page.evaluate(() => {
-        return document.body.innerText;
+    const { profileName, pageContent, socialIcons, links } = await page.evaluate(() => {
+        const nameElement = document.querySelector('#profile-title') as HTMLElement;
+        const profileName = nameElement?.innerText || 'Unknown';
+
+        const pageContent = document.body.innerText;
+
+        const socialIcons = Array.from(document.querySelectorAll('a[data-testid="SocialIcon"]')).map(icon => ({
+            title: icon.getAttribute('title') || 'Unknown',
+            url: icon.getAttribute('href') || 'Unknown',
+        }));
+
+        const links = Array.from(document.querySelectorAll('a[data-testid="LinkButton"]')).map(anchor => ({
+            title: anchor.querySelector('p')?.innerText || 'Unknown',
+            url: anchor.getAttribute('href') || 'Unknown',
+        }));
+
+        return { profileName, pageContent, socialIcons, links };
     });
-    log.info(`CONTENT: ${pageContent}`);
 
     const emailRegex = /([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/g;
-    const emails = pageContent.match(emailRegex) || [];
-    log.info(`Found ${emails.length} email(s) on ${request.url}`);
+    let emails: Set<string> = new Set(pageContent.match(emailRegex) || []);
+
+    const filteredSocialIcons = socialIcons.filter(icon => {
+        if (emailRegex.test(icon.url)) {
+            emails.add(icon.url.replace(/^mailto:/, ''));
+            return false;
+        }
+        return true;
+    });
+
+    links.forEach(link => {
+        if (emailRegex.test(link.url)) {
+            emails.add(link.url.replace(/^mailto:/, ''));
+        }
+    });
+
+    log.info(`Final merged emails: ${emails.size}`);
+    log.info(`Found ${filteredSocialIcons.length} social icons on ${request.url}`);
 
     await Dataset.pushData({
         url: request.loadedUrl,
         pageTitle,
-        emails: [...new Set(emails)],
+        profileName,
+        emails: Array.from(emails),
+        socialIcons: filteredSocialIcons,
+        profileLinks: links
     });
 });
